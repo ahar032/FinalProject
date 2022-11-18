172098ae2a23394c9cc5f19ad502c08d189da0ba
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-11-18 14:50:47 +0700

    fragment bottom nav

---
+++ app/src/main/java/com/mahar/busxhacktiv/bus/TicketsFragment.java
@@ -0,0 +1,31 @@
+package com.mahar.busxhacktiv.bus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.mahar.busxhacktiv.databinding.FragmentDashboardBinding;
+
+public class TicketsFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
