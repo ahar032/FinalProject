bad06aa5a27868ec9d8abf1f576f9be74daec9be
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-11-18 14:53:40 +0700

    fragment account

---
+++ app/src/main/java/com/mahar/busxhacktiv/bus/AccountFragment.java
@@ -0,0 +1,34 @@
+package com.mahar.busxhacktiv.bus;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mahar.busxhacktiv.R;
+import com.mahar.busxhacktiv.databinding.FragmentAccountBinding;
+import com.mahar.busxhacktiv.databinding.FragmentHomeBinding;
+
+public class AccountFragment extends Fragment {
+
+    private FragmentAccountBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
