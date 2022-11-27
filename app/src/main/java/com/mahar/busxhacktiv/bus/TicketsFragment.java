76c4bf4ebcc757b8da065eb45303c52722d6db5c
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-11-27 13:54:11 +0700

    list order ticket

--- app/src/main/java/com/mahar/busxhacktiv/bus/TicketsFragment.java
+++ app/src/main/java/com/mahar/busxhacktiv/bus/TicketsFragment.java
@@ -6,12 +6,32 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mahar.busxhacktiv.adapter.OrderAdapter;
 import com.mahar.busxhacktiv.databinding.FragmentDashboardBinding;
+import com.mahar.busxhacktiv.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketsFragment extends Fragment {
+    RecyclerView rv;
+    DatabaseReference reference;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    List<Order> list=new ArrayList<>();
 
+    OrderAdapter orderAdapter;
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -21,6 +41,37 @@ public class TicketsFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        database= FirebaseDatabase.getInstance();
+        reference= database.getReference();
+        auth=FirebaseAuth.getInstance();
+        rv=binding.rv;
+        rv.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        list=new ArrayList<>();
+        orderAdapter=new OrderAdapter(list,getContext());
+        rv.setAdapter(orderAdapter);
+        reference.child("Orders").orderByChild("userId").equalTo(auth.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                if(snapshot.exists()){
+                    for (DataSnapshot sp:snapshot.getChildren()){
+                        Order order=sp.getValue(Order.class);
+                        list.add(order);
+                    }
+                    orderAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
