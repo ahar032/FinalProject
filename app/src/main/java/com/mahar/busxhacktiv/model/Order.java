2bd1701d9d3ac6884af1f709becec0bcec1bf1f8
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-11-27 13:52:42 +0700

    tambah date

--- app/src/main/java/com/mahar/busxhacktiv/model/Order.java
+++ app/src/main/java/com/mahar/busxhacktiv/model/Order.java
@@ -1,20 +1,37 @@
 package com.mahar.busxhacktiv.model;
 
+import com.google.firebase.database.ServerValue;
+
+import java.security.Timestamp;
 import java.util.ArrayList;
 
 public class Order {
-    String amount,busId,orderId,tickets;
+    String amount,busId,orderId,tickets,userId;
+    Long date;
     ArrayList<String> descripsi = new ArrayList<String>();
-
     public Order() {
     }
 
-    public Order(String amount, String busId, String orderId, ArrayList<String> descripsi, String tickets) {
+    public Order(String amount, String busId, String orderId, String tickets, String userId, Long date, ArrayList<String> descripsi) {
         this.amount = amount;
         this.busId = busId;
         this.orderId = orderId;
+        this.tickets = tickets;
+        this.userId = userId;
+        this.date = date;
         this.descripsi = descripsi;
-        this.tickets=tickets;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getTickets() {
