24ab7af800168b15e1095a5ec7b1951907ca2465
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-12-11 16:29:23 +0700

    profile,signout

--- app/src/main/java/com/mahar/busxhacktiv/bus/AccountFragment.java
+++ app/src/main/java/com/mahar/busxhacktiv/bus/AccountFragment.java
@@ -1,22 +1,42 @@
 package com.mahar.busxhacktiv.bus;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mahar.busxhacktiv.R;
+import com.mahar.busxhacktiv.auth.SignUpActivity;
 import com.mahar.busxhacktiv.databinding.FragmentAccountBinding;
 import com.mahar.busxhacktiv.databinding.FragmentHomeBinding;
+import com.mahar.busxhacktiv.model.User;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AccountFragment extends Fragment {
 
     private FragmentAccountBinding binding;
-
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    CircleImageView civ;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -24,6 +44,48 @@ public class AccountFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        reference=database.getReference();
+
+        reference.child("User").child(auth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user=snapshot.getValue(User.class);
+
+                binding.email.setText(user.getEmail());
+                Picasso.get().load(user.getPicture())
+                        .error(R.drawable.ic_baseline_account_circle_24)
+                        .into(binding.imgProfile);
+                binding.name.setText(user.getFullname());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        binding.SignOutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(getContext()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent i=new Intent(getActivity(), SignUpActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
